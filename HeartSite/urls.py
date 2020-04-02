8b046890a318605551232f41de923f3e846a8d09
vito <viito.s@live.com>
2020-04-02 16:03:37 -0300

    Aqui va el primer commit con un buen css ! .

--- HeartSite/urls.py
+++ HeartSite/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from core import views
 
 urlpatterns = [
+    path('', views.home, name="home"),
+    path('about-me/', views.about, name="about"),
+    path('portfolio/', views.portfolio, name="portfolio"),
+    path('contact/', views.contact, name="contact"),
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,2 @@
 from django.shortcuts import render
-
-
-def post_list(request):
-    return render(request, 'blog/post_list.html', {})
-
-
-
-
-    
 # Create your views here.
